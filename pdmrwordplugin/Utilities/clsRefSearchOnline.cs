67d85b14d6abb6843b82200866ae1d2c5746a4a5
Kathiravan Govindasamy <kathirjagan1983@gmail.com>
2022-08-13 00:17:53 +0530

    13 August 2022 ********************************** Added code to get the PubXml and checking against Original reference text

--- pdmrwordplugin/Utilities/clsRefSearchOnline.cs
+++ pdmrwordplugin/Utilities/clsRefSearchOnline.cs
@@ -7,7 +7,10 @@ using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Xml.XPath;
 
 namespace pdmrwordplugin.Utilities
 {
@@ -86,6 +89,92 @@ namespace pdmrwordplugin.Utilities
             catch { return loadreference; }
         }
 
+        public static async Task<string> FetchPubmedContent(string pubids)
+        {
+            try
+            {
+                string resultJson = "";
+                string sQuery = "efetch.fcgi?db=pubmed&id=<idlist>&retmode=xml";
+                sQuery = sQuery.Replace("<idlist>", pubids);
+                HttpResponseMessage response = await searchclient.GetAsync(sQuery);
+                if (response.IsSuccessStatusCode)
+                {
+                    resultJson = await response.Content.ReadAsStringAsync();
+                }
+                return resultJson;
+            }
+            catch { return ""; }
+        }
+
+        public static string GetBetterMatchRec(string pubxml, string orgreftext)
+        {
+            try
+            {
+                string matchedelems = "";
+                var xDoc = XDocument.Parse(pubxml);
+                var articles = from item in xDoc.Descendants("PubmedArticle") select item;
+                foreach (var article in articles)
+                {
+                    var authors = from item in article.Descendants("Author") select item;
+                    foreach (var author in authors)
+                    {
+                        var lastname = author.Element("LastName");
+                        if (lastname != null)
+                        {
+                            if (orgreftext.Contains(lastname.Value)) { matchedelems += "$Authorfound$"; }
+                        }
+                    }
+                    var pubdate = from item in article.Descendants("PubDate") select item;
+                    if (pubdate != null)
+                    {
+                        var date = pubdate.FirstOrDefault().Element("Year");
+                        if (date != null)
+                        {
+                            if (orgreftext.Contains(date.Value))
+                            {
+                                matchedelems += "$YearFound$";
+                                orgreftext = orgreftext.Replace(date.Value, "");
+                            }
+                        }
+                    }
+                    var pagination = from item in article.Descendants("MedlinePgn") select item;
+                    if (pagination != null)
+                    {
+                        string firstpagenum = GetFirstPagenumber(pagination.FirstOrDefault().Value);
+                        if (orgreftext.Contains(firstpagenum + "-") || orgreftext.Contains(firstpagenum + "\u2014"))
+                        {
+                            matchedelems += "$PageFound$";
+                            orgreftext = Regex.Replace(orgreftext, firstpagenum + @"[\u2014\-]\d+", "");
+                        }
+                    }
+                    var volume = from item in article.Descendants("Volume") select item;
+                    if (volume != null)
+                    {
+                        if (orgreftext.Contains(volume.FirstOrDefault().Value))
+                        {
+                            matchedelems += "$VolumeFound$";
+                            orgreftext = orgreftext.Replace(volume.FirstOrDefault().Value, "");
+                        }
+                    }
+                }
+                return "";
+            }
+            catch { return ""; }
+        }
+
+        private static string GetFirstPagenumber(string strpage)
+        {
+            try
+            {
+                if (Regex.IsMatch(strpage, @"\d+"))
+                {
+                    return Regex.Matches(strpage, @"\d+")[0].Value;
+                }
+                return strpage;
+            }
+            catch { return strpage; }
+        }
+
         public static async Task<string> SearchPubmedOnline(ReferenceModel sreference)
         {
             try
@@ -118,13 +207,13 @@ namespace pdmrwordplugin.Utilities
                                 JObject jobj = JObject.Parse(resultJson);
                                 if (jobj.ContainsKey("esearchresult"))
                                 {   
-                                    int resultcount = JObject.Parse(resultJson)["esearchresult"].Count();
-                                    if (resultcount > 0)
+                                    var refidlists = (from p in jobj["esearchresult"]["idlist"] select (string)p).ToList();
+                                    if (refidlists.Count > 0)
                                     {
-                                        resultJson = "";
-                                        //break;
+                                        string pubmedids = string.Join(",", refidlists);
+                                        resultJson = await FetchPubmedContent(pubmedids);
+                                        string sBettermatch = GetBetterMatchRec(resultJson, prcReference.Reftext);
                                     }
-                                    //Check the conditions and break;
                                 }
                             }
                         }
